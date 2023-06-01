cacfb97b1f7450983fab8aad01c97e048d2030e0
Daniel Serrano <dserranog@unal.edu.co>
2023-06-01 18:30:57 -0500

    add sign up and login to router

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,12 +1,16 @@
 import { createRouter, createWebHistory } from "vue-router";
-import HomeComponent from "@/views/HomeView.vue";
+import HomeView from "@/views/HomeView.vue";
+import SignUpView from "@/views/SignUpView.vue";
+import LoginView from "@/views/LoginView.vue";
 
 const routes = [
   {
     name: "Home",
     path: "/",
-    component: HomeComponent,
+    component: HomeView,
   },
+  { name: "SignUp", path: "/sign-up", component: SignUpView },
+  { name: "Login", path: "/login", component: LoginView },
 ];
 
 const router = createRouter({ routes, history: createWebHistory() });
